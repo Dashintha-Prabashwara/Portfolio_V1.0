db1b14905f7d62089f5887d7c1d3850002609d10
Dashintha Jayawardana <dashikpjay@gmail.com>
2025-02-27 09:26:13 +0530

    add links and fucntionality of the form

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active');
 }
+
+document.getElementById("connectBtn").addEventListener("click", function() {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("getcv").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default behavior (form submission or page redirect)
+
+    let fileId = "1nvvn0-hNgHuaOF21VkZmQIFw8-OuE9Og"; 
+    let downloadLink = `https://drive.google.com/uc?export=download&id=${fileId}`;
+
+    let a = document.createElement("a");
+    a.href = downloadLink;
+    a.download = "CV.pdf"; 
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+});
+
